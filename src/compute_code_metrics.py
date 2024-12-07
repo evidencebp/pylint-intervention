ea013132284853d23611970147237d507543c10f
evidencebp <idanamit@gmail.com>
2024-12-07 16:57:40 +0200

    Added get_Halstead_metrics
    did a small refactor for reuse

--- src/compute_code_metrics.py
+++ src/compute_code_metrics.py
@@ -1,18 +1,37 @@
+"""
 
+Getting code metrics using Radon
+https://radon.readthedocs.io/en/latest/index.html
+"""
 from utils import run_powershell_cmd
+
 def get_raw_metrics(file: str)-> dict:
 
+    return get_metrics_set(file
+                    , type='raw'
+                    , metric_to_extract=['LOC', 'LLOC', 'SLOC', 'Comments', 'Single comments', 'Multi', 'Blank'])
+
+def get_Halstead_metrics(file: str)-> dict:
+    return get_metrics_set(file
+                    , type='hal'
+                    , metric_to_extract=['h1', 'h2', 'N1', 'N2', 'vocabulary', 'length', 'calculated_length'
+                                         , 'volume', 'difficulty', 'effort', 'time', 'bugs'])
+
+def get_metrics_set(file: str
+                    , type:str
+                    , metric_to_extract)-> dict:
+
     metrics_dict = {}
-    cmd = f'radon raw {file}'
+    cmd = f'radon {type} {file}'
 
     result = str(run_powershell_cmd(cmd).stdout)
 
-    for i in ['LOC', 'LLOC', 'SLOC', 'Comments', 'Single comments', 'Multi', 'Blank']:
+    for i in metric_to_extract:
         i_pos = result.find(i)
-        metrics_dict[i] = int(result[i_pos
+        metrics_dict[i] = result[i_pos
                                      + len(i)
                                      +2 # for ': '
-                                     : result.find('\\r\\n', i_pos)])
+                                     : result.find('\\r\\n', i_pos)]
     return metrics_dict
 
-print(get_raw_metrics("C:/src/alex-bot/alexBot/cogs/voiceCommands.py"))
+print(get_Halstead_metrics("C:/src/alex-bot/alexBot/cogs/voiceCommands.py"))
