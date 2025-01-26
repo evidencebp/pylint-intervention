6e6b4db681e3ad6b4b26bffa3df923231afc6b1d
evidencebp <idanamit@gmail.com>
2025-01-26 18:20:47 +0200

    generlize

--- src/data_cleanup.py
+++ src/data_cleanup.py
@@ -3,18 +3,20 @@ from os.path import join
 
 import pandas as pd
 
-from configuration import DONE_DIRECTORY
+from configuration import DONE_DIRECTORY, REPO_COL
 
-def clean_whitespace():
+def clean_whitespace(field):
     intervention_files = listdir(DONE_DIRECTORY)
 
     for i in intervention_files:
         df = pd.read_csv(join(DONE_DIRECTORY
                               , i))
-        df['msg'] = df['msg'].map(lambda x: x.strip())
+        df[field] = df[field].map(lambda x: str(x).strip())
         df.to_csv(join(DONE_DIRECTORY
                               , i)
                   , index=False)
 
 if __name__ == "__main__":
-    clean_whitespace()
+    #clean_whitespace(field='msg')
+    clean_whitespace(field=REPO_COL)
+
